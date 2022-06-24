6ae43bc47d0e8221780c0d3de244c9637c05e26e
YassineEF <yassineonline@outlook.fr>
2022-06-24 13:53:34 +0200

    :art: Created Header

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- META -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- TITLE -->
     <title>Addicine</title>
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500&display=swap" rel="stylesheet">
+    <!-- CSS -->
+    <link rel="stylesheet" href="./public/assets/css/main.css">
+    <!-- FONT-AWSOME -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 </head>
 <body>
-    
+    <header>
+        <div class="left">
+            <img src="./public/assets/img/Logo.png" alt="Logo Addicine">
+        </div>
+        <div class="center">
+            <ul>
+                <li><a href="#">Film</a></li>
+                <li><a href="#">Séries</a></li>
+            </ul>
+        </div>
+        <div class="right">
+            <form action="find.php" method="get" class="searchBar">
+                <input type="text" class="search" required>
+                <i class="fa fa-search"></i>
+            </form>
+        </div>
+        </header>
+        <div class="line"> </div>
+        <main>
+
+    </main>
 </body>
 </html>
